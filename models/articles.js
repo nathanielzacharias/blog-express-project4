cc08d2c2c278c9debd97ad258c0ece271fc9ec57
nathanielzacharias <nathanielzacharias@gmail.com>
2022-11-02 15:24:48 +0800

    feat: added articles.js in models folder

---
+++ models/articles.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const articleSchema = new mongoose.Schema({
+
+  title: {
+    type: String,
+  },
+
+  author: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+
+  summary: {
+    type: String,
+  },
+
+  published: {
+    type: Boolean,
+  },
+
+  body: {
+    type: String,
+  },
+
+  tags: {
+    type: [],
+  },
+
+  images: {
+    type: [],
+  },
+});
+
+const Article = mongoose.model("Article", articleSchema);
+module.exports = Article;
