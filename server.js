cb424e9be58c3a9341a400fc4381024e6215404c
nathanielzacharias <nathanielzacharias@gmail.com>
2022-11-03 22:13:38 +0800

    feat: allow cors in serves.js

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended: true})) 
 app.use(bodyParser.json()) 
 
+// allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 //routes
 const userRouter = require("./routers/userRoutes");
 const articleRouter = require("./routers/articleRoutes");
