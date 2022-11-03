20beb111aa09b4baed51942c6cafc76ac67694dc
nathanielzacharias <nathanielzacharias@gmail.com>
2022-11-03 11:41:37 +0800

    feat:added routes to publishRoutes

--- routers/publishRoutes.js
+++ routers/publishRoutes.js
@@ -4,6 +4,10 @@ const articleController = require("../controllers/articleController");
 
 
 router.post("/save", articleController.saveArticle)
+router.post("/publish", articleController.publishArticle)
+router.post("/update", articleController.updateArticle)
+router.post("/delete", articleController.deleteArticle)
+router.post("/show-all-posts", articleController.showAllPosts)
 
 
 module.exports = router;
